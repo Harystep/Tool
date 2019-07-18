b4876c47d8164a6ff4f966f9a4e704823da35400
huangran_iOS <warm588@163.com>
2019-07-18 14:34:25 +0800

    添加model

---
+++ HRCommonTool/HRModel.h
@@ -0,0 +1,17 @@
+//
+//  HRModel.h
+//  HRCommonTool
+//
+//  Created by pxsl on 2019/7/18.
+//  Copyright © 2019 CCAPP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HRModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HRCommonTool/HRModel.m
@@ -0,0 +1,13 @@
+//
+//  HRModel.m
+//  HRCommonTool
+//
+//  Created by pxsl on 2019/7/18.
+//  Copyright © 2019 CCAPP. All rights reserved.
+//
+
+#import "HRModel.h"
+
+@implementation HRModel
+
+@end
